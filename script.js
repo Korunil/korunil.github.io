31af3d880f5d86fe1cf07864eb12ec0ac2b5af0c
Anil Kumar Korupoju <anil.korupoju@outlook.com>
2026-08-03 18:17:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     });
 
+    document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
+        anchor.addEventListener("click",function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+
+            .scrollIntoView({
+
+            behavior:"smooth"
+
+            });
+
+        });
+
+    });
+
     /* -------------------------------
        Active Navigation Highlight
     -------------------------------- */
